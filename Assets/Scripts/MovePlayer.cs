115f8f8f9f2add919763ee08d9ea97c32f707d14
MagyoDev <alandelimasilva51@gmail.com>
2023-10-24 18:24:26 -0300

    dash

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,12 +4,55 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    public float speed;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb2d;
+    private Vector2 moveInput;
+
+    private float activeMoveSpeed; 
+    public float dashSpeed = 10f; 
+
+    public float dashLength = .5f, dashCooldown = 1f; 
+
+    private float dashCounter; 
+    private float dashCooldownCounter; 
+
+    private void Start()
+    {
+        activeMoveSpeed = moveSpeed; 
+    }
 
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
 
-        transform.position = transform.position + speed * move * Time.deltaTime;
+        moveInput.Normalize();
+
+        rb2d.velocity = moveInput * activeMoveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dashCooldownCounter <=0 && dashCounter <=0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCooldownCounter = dashCooldown;
+            }
+        }
+
+        if (dashCooldownCounter > 0) 
+        { 
+            dashCooldownCounter -= Time.deltaTime;
+        }
     }
 }
